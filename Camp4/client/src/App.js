7e83423302217d308e1b1a7d0fc1a6c86068666d
Nicholas Douso <nicholas.douso@gmail.com>
2021-05-04 08:53:31 -0500

    starting over

--- Camp4/client/src/App.js
+++ Camp4/client/src/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
+import logo from './logo.svg';
 import './App.css';
-import { UserProfileProvider } from "./providers/UserProfileProvider";
-import Header from "./components/Header";
-import ApplicationViews from "./components/ApplicationViews";
 
 function App() {
   return (
-    <Router>
-      <UserProfileProvider>
-          <Header />
-          <ApplicationViews />    
-      </UserProfileProvider>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
